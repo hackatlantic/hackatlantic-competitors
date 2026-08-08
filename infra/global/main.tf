ea2615e59edd1a8033be22e185704bbbdc56584b
Adebayo Adebowale <adebowale.ca@gmail.com>
2026-08-07 23:36:24 -0400

    Align repository security gates

--- infra/global/main.tf
+++ infra/global/main.tf
@@ -90,6 +90,7 @@ resource "github_branch_protection" "main" {
   enforce_admins                  = true
   allows_deletions                = false
   allows_force_pushes             = false
+  require_signed_commits          = true
   required_linear_history         = true
   require_conversation_resolution = true
 
