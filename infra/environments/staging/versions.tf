32a455c26afca1d3edab670a38c4e4b0c8587cea
Adebayo Adebowale <adebowale.ca@gmail.com>
2026-08-11 10:52:40 -0400

    Generate valid staging pass peppers

--- infra/environments/staging/versions.tf
+++ infra/environments/staging/versions.tf
@@ -3,10 +3,10 @@ terraform {
   backend "remote" {}
   required_providers {
     digitalocean = { source = "digitalocean/digitalocean", version = "~> 2.96" }
+    random       = { source = "hashicorp/random", version = "~> 3.7" }
     supabase     = { source = "supabase/supabase", version = "~> 1.9" }
   }
 }
 
 provider "digitalocean" {}
 provider "supabase" {}
-
