503f9dbdb3580aa92e91120802e9ac1ebd80f6be
Dax Manuel <dax.manuel@unb.ca>
2026-08-26 14:06:57 -0300

    Fix application question max words type

--- lib/api.ts
+++ lib/api.ts
@@ -45,6 +45,7 @@ export type ApplicationFormQuestion = {
   type: ApplicationQuestionType;
   required: boolean;
   help?: string;
+  maxWords?: number;
 };
 
 export type CurrentApplicationForm = {
